f73b785da49735ec84b8bbaab835d820be00307a
Zübeyir Bodur <zubeyir99@hotmail.com>
2021-12-22 22:02:23 +0300

    Disabled 2 buttons that wont be used in first iteration

--- app/src/main/java/com/example/deepaint/MainActivity.java
+++ app/src/main/java/com/example/deepaint/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.deepaint;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button openCamera;
+    private Button savedObjects;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
+        //openCamera.setEnabled(false);
+        //savedObjects.setEnabled(false);
+
         setContentView(R.layout.activity_main);
     }
 }
